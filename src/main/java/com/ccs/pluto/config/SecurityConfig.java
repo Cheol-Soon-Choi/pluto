8d09179867e9887ecb9c28ff25ece6beb9db925d
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2021-11-04 18:44:04 +0900

    CHORE: Add security dependency

---
+++ src/main/java/com/ccs/pluto/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ccs.pluto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/com/ccs/pluto/models/dto/ItemDto.java
@@ -0,0 +1,20 @@
+package com.ccs.pluto.models.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemDto {
+
+    private Long id;
+
+    private String itemName;
+
+    private int price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+}
