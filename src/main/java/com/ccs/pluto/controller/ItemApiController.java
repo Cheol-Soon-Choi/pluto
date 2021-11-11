197f088ca53c2bace43ef3e4415c44d34a8683d9
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2021-11-11 21:23:06 +0900

    FEAT: Add ItemApiController(Post)

---
+++ src/main/java/com/ccs/pluto/controller/ItemApiController.java
@@ -0,0 +1,47 @@
+package com.ccs.pluto.controller;
+
+import com.ccs.pluto.models.dto.ItemFormDto;
+import com.ccs.pluto.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ItemApiController {
+
+    private final ItemService itemService;
+
+    @PostMapping("/admin/items")
+    public ModelAndView newItem(@Valid ItemFormDto itemFormDto, BindingResult bindingResult,
+                                List<MultipartFile> itemImgFileList) {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/index");
+
+        if (bindingResult.hasErrors()) {
+            mav.setViewName("/item/itemForm");
+            return mav;
+        }
+
+        if (itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
+            mav.setViewName("/item/itemForm");
+            mav.addObject("errorMessage", "첫번째 상품 이미지는 필수 입력 값 입니다.");
+            return mav;
+        }
+
+        try {
+            itemService.saveItem(itemFormDto, itemImgFileList);
+        } catch (Exception e) {
+            mav.setViewName("/item/itemForm");
+            mav.addObject("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
+            return mav;
+        }
+        return mav;
+    }
+}
