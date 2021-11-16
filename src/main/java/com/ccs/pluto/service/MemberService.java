9bff0330ee21968e06cd006a1da099f03181f25d
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2021-11-16 21:03:09 +0900

    FIX: Fix getter typo

--- src/main/java/com/ccs/pluto/service/MemberService.java
+++ src/main/java/com/ccs/pluto/service/MemberService.java
@@ -41,7 +41,7 @@ public class MemberService implements UserDetailsService {
         }
 
         return User.builder()
-                .username(member.getName())
+                .username(member.getEmail())
                 .password(member.getPassword())
                 .roles(member.getRole().toString())
                 .build();
