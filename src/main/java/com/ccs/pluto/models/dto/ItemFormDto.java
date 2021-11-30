d255f186ecf0c06074ccd4bde2e96b3c11a21716
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2021-11-30 22:28:17 +0900

    FIX: Add valid message to Integer variables

--- src/main/java/com/ccs/pluto/models/dto/ItemFormDto.java
+++ src/main/java/com/ccs/pluto/models/dto/ItemFormDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.modelmapper.ModelMapper;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
@@ -21,12 +22,14 @@ public class ItemFormDto {
     private String itemName;
 
     @NotNull(message = "가격은 필수 입력 값입니다.")
+    @Min(value = 0, message = "최소 가격은 0원 이상입니다.")
     private Integer price;
 
     @NotBlank(message = "상품 상세는 필수 입력 값입니다.")
     private String itemDetail;
 
     @NotNull(message = "재고는 필수 입력 값입니다.")
+    @Min(value = 0, message = "최소 재고는 0개 이상입니다.")
     private Integer stockNumber;
 
     private ItemSellStatus itemSellStatus;
